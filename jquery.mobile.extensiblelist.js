9409240b24beee9f21006a10fe2f9b7804d4d801
Volker Krebs <volker.krebs@abas.de>
2012-07-19 15:12:28 +0200

    First working version

--- jquery.mobile.extensiblelist.js
+++ jquery.mobile.extensiblelist.js
@@ -5,9 +5,15 @@
 		},
 		_create: function() {
 			//TODO: Check if listview is ok, are we really an ul element ?
-			this.element.append('<li>' + this.options.placeholder + '</li>');
+			this.element.append('<li class="ui-extensiblelist-morebtn"><a href="#" class="extensiblelist-morebtn">' + this.options.placeholder + '</a></li>');
 			this.element.listview();
+			$(".extensiblelist-morebtn").bind("click", this.element, this._more)
 		},
+		_more: function(event) {
+			var morebtn = event.data.children(".ui-extensiblelist-morebtn");
+			$('<li>mmmmm</li>').insertBefore(morebtn);
+			event.data.listview('refresh');
+		}
 	});
 
 	$(document).bind("pagecreate", function(event) {
