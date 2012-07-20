c9252ec8b05984023370c10057c12cea89c5cfd9
Volker Krebs <volker.krebs@abas.de>
2012-07-20 09:50:13 +0200

    Working non-ajax version.

--- jquery.mobile.extensiblelist.js
+++ jquery.mobile.extensiblelist.js
@@ -1,18 +1,32 @@
 (function($){
 	$.widget("mobile.extensiblelist", $.mobile.widget, {
 		options: {
-			placeholder: 'more ...',
+			extlstPlaceholder: 'more',
+			extlstSize: 5,
 		},
 		_create: function() {
+			var listitems = this.element.children('li');
+			listitems.each(function(index, li) {
+				if(index >= 3) {
+					$(li).hide();
+				}
+			});
 			//TODO: Check if listview is ok, are we really an ul element ?
-			this.element.append('<li class="ui-extensiblelist-morebtn"><a href="#" class="extensiblelist-morebtn">' + this.options.placeholder + '</a></li>');
+			this.element.append('<li class="ui-extensiblelist-morebtn"><a href="#" class="extensiblelist-morebtn">' + this.options.extlstPlaceholder + '</a></li>');
 			this.element.listview();
 			$(".extensiblelist-morebtn").bind("click", this.element, this._more)
 		},
 		_more: function(event) {
 			var morebtn = event.data.children(".ui-extensiblelist-morebtn");
-			$('<li>mmmmm</li>').insertBefore(morebtn);
-			event.data.listview('refresh');
+			var hiddenListitems = event.data.children('li:hidden');
+			hiddenListitems.each(function(index, li) {
+				if(index < 3) {
+					$(li).show();
+				}
+			});
+			if (hiddenListitems.length <= 3) {
+				morebtn.remove();
+			}
 		}
 	});
 
