c59c79f67ffc81585fe6ee82d2da5897ff2a2214
Stefan Gebhardt <stefan.gebhardt@finux.net>
2012-07-24 17:48:31 +0200

    Added a count bubble of hidden elemnts to th more button. added style to morebuttn and a  bit of refactoring

--- jquery.mobile.extensiblelist.js
+++ jquery.mobile.extensiblelist.js
@@ -19,8 +19,9 @@
 				//default to 5 if input was wrong
 				this.options.extlstSize = 5;
 			}
-			var self = this;
-			var appendMoreButton = false;
+			var self = this
+			  , appendMoreButton = false
+			  , hiddenEntriesCount = listitems.length - this.options.extlstSize;
 			listitems.each(function(index, li) {
 				if(index >= self.options.extlstSize) {
 					$(li).hide();
@@ -28,14 +29,20 @@
 				}
 			});
 			if (appendMoreButton) {
-				this.element.append('<li class="ui-extensiblelist-morebtn"><a href="#" class="extensiblelist-morebtn">' + this.options.extlstPlaceholder + '</a></li>');
-				this.element.find(".extensiblelist-morebtn").bind("click", { list : this.element , size : this.options.extlstSize } , this._more)
+				var countBubble = $('<span class="ui-li-count">').html(hiddenEntriesCount)
+				  , btninner = $('<a href="#" class="extensiblelist-morebtn">')
+					.bind("click", {list: this.element, size: this.options.extlstSize} , this._more)
+				  	.html(this.options.extlstPlaceholder)
+				  	.append(countBubble)
+				var morebtn = $('<li data-icon="false" class="ui-extensiblelist-morebtn">').append(btninner);
+				this.element.append(morebtn);
 			}
 			this.element.listview();
 		},
 		_more: function(event) {
-			var morebtn = event.data.list.children(".ui-extensiblelist-morebtn");
-			var hiddenListitems = event.data.list.children('li:hidden');
+			var morebtn = event.data.list.children(".ui-extensiblelist-morebtn")
+			  , hiddenListitems = event.data.list.children('li:hidden')
+			  , countBubble = morebtn.find('span.ui-li-count');
 			hiddenListitems.each(function(index, li) {
 				if(index < event.data.size) {
 					$(li).show();
@@ -44,6 +51,7 @@
 			if (hiddenListitems.length <= event.data.size) {
 				morebtn.remove();
 			}
+			countBubble.html(hiddenListitems.length - event.data.size);
 		}
 	});
 
