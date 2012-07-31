c8fb5f601b34b4ad2319fdc54f165900b54447a1
Volker Krebs <volker.krebs@abas.de>
2012-07-31 17:19:59 +0200

    Use qunit-fixture for markup that should be tested.

--- tests/jquery.mobile.extensiblelist.test.js
+++ tests/jquery.mobile.extensiblelist.test.js
@@ -6,23 +6,19 @@ test('test proper setup', 3, function() {
 	strictEqual(typeof jQuery.mobile.extensiblelist, 'function', 'jquery mobile extensiblelist initialized');
 });
 
-test('test plug-in', 3, function() {
-	var boilerplate = $([
-					'<div data-role="page">',
-						'<div data-role="content"></div>',
-					'</div>'].join(''));
-	boilerplate.appendTo( document.body );
+test('test plug-in with default values', 5, function() {
 	var items;
 	for (i=0; i<15; i++) {
 		items += '<li>item' + i + '</li>'
 	}
 	var list = jQuery( '<ul data-role="extensiblelist">' + items + '</ul>' ).appendTo( ':jqmData(role=content)' );
-	boilerplate.page();
 	list.extensiblelist();
 	equal(jQuery(':jqmData(role=extensiblelist) > li').length, 16, 'Total of 16 li expected. 15 items, one more button');
 	equal(jQuery('.ui-extensiblelist-morebtn').length, 1, 'Expecting 1 more button');
 	equal(jQuery('span.ui-li-count').text(), 10, '10 items left');
-	//the selector li:hidden is not working in test environment
-	//jQuery('a.extensiblelist-morebtn').trigger('click');
-	//equal(jQuery('span.ui-li-count').text(), 5, '5 items left');
+	jQuery('a.extensiblelist-morebtn').trigger('click');
+	equal(jQuery('span.ui-li-count').text(), 5, '5 items left');
+	jQuery('a.extensiblelist-morebtn').trigger('click');
+	equal(jQuery('.ui-extensiblelist-morebtn').length, 0, 'No more button left');
 });
+
