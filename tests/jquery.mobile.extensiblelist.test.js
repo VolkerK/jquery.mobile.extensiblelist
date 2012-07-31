b2b8662f4e184ab50c3df4fe1e12a45b378bede3
Volker Krebs <volker.krebs@abas.de>
2012-07-31 16:05:51 +0200

    More tests

--- tests/jquery.mobile.extensiblelist.test.js
+++ tests/jquery.mobile.extensiblelist.test.js
@@ -1,12 +1,12 @@
 module('jquery mobile extensible list test');
 
 test('test proper setup', 3, function() {
-	strictEqual(typeof jQuery, 'function', 'jquery not initialized');
-	strictEqual(typeof jQuery.mobile, 'object', 'jquery mobile not initialized');
-	strictEqual(typeof jQuery.mobile.extensiblelist, 'function', 'jquery mobile extensiblelist not initialized');
+	strictEqual(typeof jQuery, 'function', 'jquery initialized');
+	strictEqual(typeof jQuery.mobile, 'object', 'jquery mobile initialized');
+	strictEqual(typeof jQuery.mobile.extensiblelist, 'function', 'jquery mobile extensiblelist initialized');
 });
 
-test("test plug-in", 0, function() {
+test('test plug-in', 3, function() {
 	var boilerplate = $([
 					'<div data-role="page">',
 						'<div data-role="content"></div>',
@@ -17,8 +17,12 @@ test("test plug-in", 0, function() {
 		items += '<li>item' + i + '</li>'
 	}
 	var list = jQuery( '<ul data-role="extensiblelist">' + items + '</ul>' ).appendTo( ':jqmData(role=content)' );
-	//var e = jQuery( '#test1' );
-	//strictEqual(typeof e, "object", "no element found");
 	boilerplate.page();
 	list.extensiblelist();
+	equal(jQuery(':jqmData(role=extensiblelist) > li').length, 16, 'Total of 16 li expected. 15 items, one more button');
+	equal(jQuery('.ui-extensiblelist-morebtn').length, 1, 'Expecting 1 more button');
+	equal(jQuery('span.ui-li-count').text(), 10, '10 items left');
+	//the selector li:hidden is not working in test environment
+	//jQuery('a.extensiblelist-morebtn').trigger('click');
+	//equal(jQuery('span.ui-li-count').text(), 5, '5 items left');
 });
